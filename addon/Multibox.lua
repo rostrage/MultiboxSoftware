20bc36e71d1e328f90f7e51dbb457612cc46802f
ryan <ryan.ostrager@gmail.com>
2025-11-09 06:15:27 -0500

    Adds initial broadcasting implementation

--- addon/Multibox.lua
+++ addon/Multibox.lua
@@ -1,7 +1,7 @@
-SLASH_MBOX1 = '/mbox';
-local controlCommand = 0
-local keysEnabled = true
-local broadcastEnabled = false
+local Multibox = LibStub("AceAddon-3.0"):NewAddon("Multibox", "AceComm-3.0")
+
+local MESSAGE_PREFIX = "MBX"
+
 -- Frame for drawing
 local frame = CreateFrame("Frame", nil, UIParent)
 frame:SetPoint("TOPLEFT", 0, 0)
@@ -20,10 +20,19 @@ sentinelTexture:SetPoint("TOPLEFT", 1, 0)
 sentinelTexture:SetSize(1, 1)
 sentinelTexture:SetTexture("Interface\\AddOns\\Multibox\\Smooth.tga")
 
-local getNextMacro = RestoShaman.getRestoShamanMacro;
+local getNextMacro
+
 -- Draws a single pixel with given RGB values (0-1 range)
 local function drawPixel(r, g, b)
     commandTexture:SetVertexColor(r, g, b)
+end
+
+function Multibox:OnInitialize()
+    self.keysEnabled = true
+    self.broadcastEnabled = false
+    self.controlCommand = 0
+    self:RegisterComm(MESSAGE_PREFIX)
+    self:Init()
 end
 
 -- OnUpdate loop to draw the key value as a pixel color
@@ -32,15 +41,16 @@ frame:HookScript("OnUpdate", function(self, elapsed)
     timeElapsed = timeElapsed + elapsed
     if (timeElapsed > 0.1) then
         timeElapsed = 0
-
-        local key, target = getNextMacro()
-        -- Normalize the key to a value between 0 and 1 using 255 as the max for 8-bit color
-        local r = key / 255
-        local g = target / 255
-        local b = controlCommand / 255
-        drawPixel(r, g, b)
-        if controlCommand ~= 0 then
-            controlCommand = 0
+        if getNextMacro then
+            local key, target = getNextMacro()
+            -- Normalize the key to a value between 0 and 1 using 255 as the max for 8-bit color
+            local r = key / 255
+            local g = target / 255
+            local b = Multibox.controlCommand / 255
+            drawPixel(r, g, b)
+            if Multibox.controlCommand ~= 0 then
+                Multibox.controlCommand = 0
+            end
         end
     end
 end)
@@ -79,18 +89,10 @@ local function initTargettingKeybinds()
         else
             button:SetAttribute("macrotext", "/target raid" .. i)
         end
-
-        -- Bind the key to this button
     end
 end
 
-function SlashCmdList.MBOX_INIT(msg, editBox) -- 4.
-    -- Initialize configuration system
-    MultiboxConfig:RegisterOptions()
-end
-
-local CURRENT_SPEC = "Unsupported"
-local function init(msg, editBox)
+function Multibox:Init()
     initTargettingKeybinds();
     MultiboxGuildBank:Initialize()
     MultiboxFollow:Initialize()
@@ -107,54 +109,41 @@ local function init(msg, editBox)
             DEFAULT_CHAT_FRAME:AddMessage("INIT RETRI PALADIN");
             RetriPaladin.initRetriPaladinKeybinds();
             getNextMacro = RetriPaladin.getRetriPaladinMacro;
-            CURRENT_SPEC = "Retribution Paladin"
-            
-            -- Initialize Retribution Paladin configuration
-            RetriPaladinDB = RetriPaladinDB or { judgmentType = "light" }
-            RetriPaladin:UpdateJudgmentMacro()
         end
         _, _, _, _, currentRank = GetTalentInfo(1, 26)
         if currentRank == 1 then
             DEFAULT_CHAT_FRAME:AddMessage("INIT HOLY PALADIN");
             HolyPaladin.initHolyPaladinKeybinds();
             getNextMacro = HolyPaladin.getHolyPaladinMacro;
-            CURRENT_SPEC = "Holy Paladin"
         end
     elseif playerClass == "Shaman" then
         DEFAULT_CHAT_FRAME:AddMessage("INIT RESTO SHAMAN");
         RestoShaman.initRestoShamanKeybinds();
         getNextMacro = RestoShaman.getRestoShamanMacro;
-        CURRENT_SPEC = "Restoration Shaman"
     elseif playerClass == "Druid" then
         _, _, _, _, currentRank = GetTalentInfo(1, 28);
         if currentRank == 1 then
             DEFAULT_CHAT_FRAME:AddMessage("INIT BALANCE DRUID");
             BalanceDruid.initBalanceDruidKeybinds();
             getNextMacro = BalanceDruid.getBalanceDruidMacro;
-            CURRENT_SPEC = "Balance Druid"
         end
-        -- -- Feral cat - Feral Aggression rank 5
         _, _, _, _, currentRank = GetTalentInfo(2, 2);
         if currentRank == 5 then
             DEFAULT_CHAT_FRAME:AddMessage("INIT FERAL CAT DRUID");
             FeralCatDruid.initFeralCatDruidKeybinds();
             getNextMacro = FeralCatDruid.getFeralCatDruidMacro;
-            CURRENT_SPEC = "Feral (Cat) Druid"
         end
-        -- Feral bear - Thick Hide rank 3
         _, _, _, _, currentRank = GetTalentInfo(2, 5);
         if currentRank == 3 then
             DEFAULT_CHAT_FRAME:AddMessage("INIT FERAL BEAR DRUID");
             FeralBearDruid.initFeralBearDruidKeybinds();
             getNextMacro = FeralBearDruid.getFeralBearDruidMacro;
-            CURRENT_SPEC = "Feral (Bear) Druid"
         end
         _, _, _, _, currentRank = GetTalentInfo(3, 27);
         if currentRank == 1 then
             DEFAULT_CHAT_FRAME:AddMessage("INIT RESTO DRUID");
             RestoDruid.initRestoDruidKeybinds();
             getNextMacro = RestoDruid.getRestoDruidMacro;
-            CURRENT_SPEC = "Restoration Druid"
         end
     elseif playerClass == "Priest" then
         _, _, _, _, currentRank = GetTalentInfo(3, 27);
@@ -162,7 +151,6 @@ local function init(msg, editBox)
             DEFAULT_CHAT_FRAME:AddMessage("INIT SHADOW PRIEST");
             ShadowPriest.initShadowPriestKeybinds();
             getNextMacro = ShadowPriest.getShadowPriestMacro;
-            CURRENT_SPEC = "Shadow Priest"
         end
     elseif playerClass == "Warlock" then
         _, _, _, _, currentRank = GetTalentInfo(2, 27);
@@ -170,53 +158,115 @@ local function init(msg, editBox)
             DEFAULT_CHAT_FRAME:AddMessage("INIT DEMONOLOGY WARLOCK");
             DemonologyWarlock.initDemonologyWarlockKeybinds();
             getNextMacro = DemonologyWarlock.getDemonologyWarlockMacro;
-            CURRENT_SPEC = "Demonology Warlock"
         end
     else
         DEFAULT_CHAT_FRAME:AddMessage("CLASS NOT SUPPORTED");
     end
-    MultiboxConfig:UpdateSpecStatus(CURRENT_SPEC)
     drawPixel(0, 0, 0)
     sentinelTexture:SetVertexColor(0x12 / 255, 0x34 / 255, 0x56 / 255)
 end
 
-local function MboxCommandHandler(msg, editBox)
+function Multibox:MboxCommandHandler(msg)
     local cmd, args = msg:match("^(%S*)%s*(.-)$")
     cmd = cmd and string.lower(cmd) or ""
 
     if cmd == "toggle" then
-        keysEnabled = not keysEnabled
-        if keysEnabled then
-            controlCommand = 2 -- enable
+        self.keysEnabled = not self.keysEnabled
+        if self.keysEnabled then
+            self.controlCommand = 2 -- enable
             DEFAULT_CHAT_FRAME:AddMessage("Keys enabled")
         else
-            controlCommand = 1 -- disable
+            self.controlCommand = 1 -- disable
             DEFAULT_CHAT_FRAME:AddMessage("Keys disabled")
         end
+        self:SendToggle()
     elseif cmd == "broadcast" then
-        broadcastEnabled = not broadcastEnabled
-        if broadcastEnabled then
-            controlCommand = 3 -- enable broadcast
+        self.broadcastEnabled = not self.broadcastEnabled
+        if self.broadcastEnabled then
+            self.controlCommand = 3 -- enable broadcast
             DEFAULT_CHAT_FRAME:AddMessage("Broadcast enabled")
         else
-            controlCommand = 4 -- disable broadcast
+            self.controlCommand = 4 -- disable broadcast
             DEFAULT_CHAT_FRAME:AddMessage("Broadcast disabled")
         end
+        self:SendBroadcast()
     elseif cmd == "swap" then
         local target = tonumber(args)
         if target and target > 0 then
-            controlCommand = target + 4
+            self.controlCommand = target + 4
             DEFAULT_CHAT_FRAME:AddMessage("Signaling swap with window " .. target)
+            self:SendSwap(target)
         else
             DEFAULT_CHAT_FRAME:AddMessage("Invalid swap target. Usage: /mbox swap <window_number>")
         end
     elseif cmd == "" then
-        init(msg, editBox)
+        self:Init()
     else
         DEFAULT_CHAT_FRAME:AddMessage("Unknown mbox command: " .. cmd)
         DEFAULT_CHAT_FRAME:AddMessage("Usage: /mbox [toggle|broadcast|swap <target>]")
     end
 end
 
-SlashCmdList["MBOX"] = MboxCommandHandler; -- Also a valid assignment strategy
--- init()
+function Multibox:OnCommReceived(prefix, message, distribution, sender)
+    if prefix ~= MESSAGE_PREFIX then
+        return
+    end
+
+    local command, value = message:match("^(%S*)%s*(.-)$")
+    command = command and string.lower(command) or ""
+
+    if command == "toggle" then
+        local newState = (value == "true")
+        if self.keysEnabled ~= newState then
+            self.keysEnabled = newState
+            if self.keysEnabled then
+                self.controlCommand = 2 -- enable
+                DEFAULT_CHAT_FRAME:AddMessage("Keys enabled by " .. sender)
+            else
+                self.controlCommand = 1 -- disable
+                DEFAULT_CHAT_FRAME:AddMessage("Keys disabled by " .. sender)
+            end
+        end
+    elseif command == "broadcast" then
+        local newState = (value == "true")
+        if self.broadcastEnabled ~= newState then
+            self.broadcastEnabled = newState
+            if self.broadcastEnabled then
+                self.controlCommand = 3 -- enable broadcast
+                DEFAULT_CHAT_FRAME:AddMessage("Broadcast enabled by " .. sender)
+            else
+                self.controlCommand = 4 -- disable broadcast
+                DEFAULT_CHAT_FRAME:AddMessage("Broadcast disabled by " .. sender)
+            end
+        end
+    elseif command == "swap" then
+        local target = tonumber(value)
+        if target and target > 0 then
+            self.controlCommand = target + 4
+            DEFAULT_CHAT_FRAME:AddMessage("Received swap signal from " .. sender .. " for window " .. target)
+        end
+    end
+end
+
+function Multibox:SendToggle()
+    local message = "toggle " .. tostring(self.keysEnabled)
+    self:SendCommMessage(MESSAGE_PREFIX, message, "RAID", nil)
+    self:SendCommMessage(MESSAGE_PREFIX, message, "PARTY", nil)
+end
+
+function Multibox:SendBroadcast()
+    local message = "broadcast " .. tostring(self.broadcastEnabled)
+    self:SendCommMessage(MESSAGE_PREFIX, message, "RAID", nil)
+    self:SendCommMessage(MESSAGE_PREFIX, message, "PARTY", nil)
+end
+
+function Multibox:SendSwap(target)
+    local message = "swap " .. tostring(target)
+    self:SendCommMessage(MESSAGE_PREFIX, message, "RAID", nil)
+    self:SendCommMessage(MESSAGE_PREFIX, message, "PARTY", nil)
+end
+
+SLASH_MBOX1 = '/mbox'
+function SlashCmdList.MBOX(msg)
+    Multibox:MboxCommandHandler(msg)
+end
