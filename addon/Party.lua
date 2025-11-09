3c1f7a5729022a38fba8f5973fe687e11bfe02de
ryan <ryan.ostrager@gmail.com>
2025-11-09 09:20:34 -0500

    Hide party invite after accepting.

--- addon/Party.lua
+++ addon/Party.lua
@@ -12,5 +12,16 @@ function MultiboxParty.OnEvent(self, event, ...)
     if event == "PARTY_INVITE_REQUEST" then
         local inviter, unk = ...
         AcceptGroup()
+        -- Make sure the invite dialog does not decline the invitation when hidden.
+		for iteratePopups = 1, STATICPOPUP_NUMDIALOGS do
+			local dialog = _G["StaticPopup"..iteratePopups]
+			if dialog.which == "PARTY_INVITE" then
+				-- Set the inviteAccepted flag to true (even if the invite was declined, as the
+				-- flag is only set to stop the dialog from declining in its OnHide event).
+				dialog.inviteAccepted = 1
+				break
+			end
+		end
+		StaticPopup_Hide( "PARTY_INVITE" )
     end
 end
