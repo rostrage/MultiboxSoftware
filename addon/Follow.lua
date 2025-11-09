6df1c944cf51f25c1dfc52d0802a1dfad7eabb27
ryan <ryan.ostrager@gmail.com>
2025-11-09 10:45:58 -0500

    Adds autofollow.

--- addon/Follow.lua
+++ addon/Follow.lua
@@ -2,15 +2,20 @@
 
 MultiboxFollow = {}
 
-function MultiboxFollow:Initialize()
-    SLASH_FOLLOW1 = "/followme"
-    SlashCmdList["FOLLOW"] = MultiboxFollow.FollowMe
+
+
+function MultiboxFollow.OnEvent(self, event, ...)
+    -- local master = UnitName("player")
+    -- if msg and msg ~= "" then
+    --     master = msg
+    -- end
+    FollowUnit("focus")
 end
 
-function MultiboxFollow.FollowMe(msg, editbox)
-    local master = UnitName("player")
-    if msg and msg ~= "" then
-        master = msg
-    end
-    FollowUnit(master)
+function MultiboxFollow:Initialize()
+    local frame = CreateFrame("Frame")
+	frame:RegisterEvent( "PLAYER_REGEN_DISABLED" )
+	frame:RegisterEvent( "PLAYER_REGEN_ENABLED" )	
+	frame:RegisterEvent( "PLAYER_CONTROL_GAINED" )
+    frame:SetScript("OnEvent", MultiboxFollow.OnEvent)
 end
